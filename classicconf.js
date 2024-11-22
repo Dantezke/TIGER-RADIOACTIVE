893a2fc2f4b4a06764793cb7ae6435a9e03923d5
Dantezke <grantonjambia@gmail.com>
2024-11-22 18:45:21 +0300

    Update classicconf.js

--- classicconf.js
+++ classicconf.js
@@ -28,19 +28,19 @@ global.region = 'kenya' // serah
 //—————「 Set Nama Own & Bot 」—————//
 global.ownername = 'DRAGON' //ubah jadi nama mu, note tanda ' gausah di hapus!
 //=================================================//
-global.owner = ['254746764468'] // ubah aja pake nomor lu
+global.owner = ['254115770750'] // ubah aja pake nomor lu
 //==========================zetsuboclient=======================//
 global.botname = '𝚻𝚰𝐆𝚵𝚪 𝐑𝚫𝐃𝚰𝚯𝚫𝐂𝚻𝚰𝛁𝚵 𝛁1' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
 global.packname = '𝚻𝚰𝐆𝚵𝚪 𝐑𝚫𝐃𝚰𝚯𝚫𝐂𝚻𝚰𝛁𝚵 𝛁1' // ubah aja ini nama sticker
 global.author = '༻࿇𝚻𝚮𝚵𝚵 𝐃𝚪𝚫𝐆𝚯𝚴❖༺' // ubah aja ini nama sticker
-global.prefa = '.'
+global.prefa = '∆'
 global.sessionName = 'Tigersession'  //Gausah Juga
 global.sp = '⭔' // Gausah Juga
 global.autoviewstatus = process.env.autoviewstatus || "TRUE"
 global.autolike = process.env.autolikestatus || "TRUE"
 global.autoread = process.env.autoread || "TRUE"
 global.wlcm = []
-global.wlcmm = []
+global.wlcmm = [true]
 global.anticall = true
 global.rn = 'false'
 /*
